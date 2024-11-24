a46bd832df24a7f52b9617de7ba1cae88e70d603
miwkom <166976262+miwkom@users.noreply.github.com>
2024-11-24 10:20:17 +0300

    Add files via upload

---
+++ module 18/UrbanDjango/UrbanDjango/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for UrbanDjango project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+from task2.views import func_view, class_view
+from task4 import views
+from task5.views import sign_up_by_django, sign_up_by_html
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('func/', func_view),
+    path('class/', class_view.as_view()),
+    path('class2/', TemplateView.as_view(template_name='class_template.html')),
+    path('platform/', views.platform),
+    path('platform/games/', views.games),
+    path('platform/cart/', views.cart),
+    path('registration/', sign_up_by_html),
+    path('registration2/', sign_up_by_django),
+]
